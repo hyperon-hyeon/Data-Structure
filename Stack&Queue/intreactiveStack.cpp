31d38a353560769b63130a9bf5096dcdbd02bad6
hyperon-hyeon <hyperon1122@gmail.com>
2025-04-18 08:42:36 +0900

    interactiveStack fix

--- Stack&Queue/intreactiveStack.cpp
+++ Stack&Queue/intreactiveStack.cpp
@@ -1,68 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX_SIZE 10
 
-int top = -1;
 int stack[MAX_SIZE];
+int top = -1;
 
-int stack_empty() {
-	if (top < 0) {	return 1;}
-	else { return 0; }
+int stackEmpty() {
+    if (top < 0) return 1;
+    else return 0;
 }
 
-int stack_full() {
-	if (top >= MAX_SIZE-1) { return 1; }
-	else { return 0; }
+int stackFull() {
+    if (top >= MAX_SIZE - 1) return -1;
+    else return 0;
 }
 
 void enStack(int num) {
-	if (!stack_full()) {
-		top++;
-		stack[top] = num;
-	}
-	else { printf("[Error] Stack is full\n"); }
+    if (!stackFull()) {
+        top++;
+        stack[top] = num;
+    }
+    else { printf("[Error] Stack is full!\n"); }
 }
 
 void deStack() {
-	if (!stack_empty()) { top--; }
-	else { printf("[Error] Stack is empty\n"); }
+    if (!stackEmpty()) top--;
+    else { printf("[Error] Stacl is empty!\n"); }
 }
 
 void printStack() {
-	printf("Current Stack: ");
-	if (!stack_empty()) {
-		for (int i = 0; i <= top; i++) {
-			printf("%d ", stack[i]);
-		}
-	} else { printf("None"); }
-	printf("\n");
+    printf("Stack ->  ");
+    if (stackEmpty()) { printf("None"); }
+    else {
+        for (int i = 0; i <= top; i++) {
+            printf("%d ", stack[i]);
+        }
+    }
+    printf("\n");
 }
 
 int main() {
-	int menuNum = -1;
-	int insertNum = -1;
 
-	printf("[Menu]: Quit - 0 / Insert - 1 / Delete - 2 / Print - 3\n");
-	printf("Press your choice: ");
-	scanf_s("%d", &menuNum);
+    int menu = -1, inputNum = 0;
+    printf("[Menu] Exit - 0 / Print - 1 / Add - 2 / Delete - 3\n");
+    scanf_s("%d", &menu);
 
-	while (menuNum != 0) {
-		if (menuNum == 1) {
-			printf("Press the number to Insert: ");
-			scanf_s("%d",&insertNum);
-			enStack(insertNum);
-			printf("[Result] ");
-			printStack();
-		}
-		else if (menuNum == 2) {
-			deStack();
-			printf("[Result] ");
-			printStack();
-		}
-		else if (menuNum == 3) { printStack(); }
-		else { printf("Press another number"); }
+    while (menu != 0) {
+        if (menu == 1) { printStack(); }
+        else if (menu == 2) {
+            printf("Insert number: ");
+            scanf_s("%d", &inputNum);
+            enStack(inputNum);
+            printf("[Result] ");
+            printStack();
+        }
+        else if (menu == 3) {
+            deStack();
+            printf("[Result] ");
+            printStack();
+        }
+        else { printf("[Error] %d is not available\n", menu); }
 
-		printf("\n[Menu]: Quit - 0 / Insert - 1 / Delete - 2 / Print - 3\n");
-		printf("Press your choice: ");
-		scanf_s("%d", &menuNum);
-	}
+        printf("\n[Menu] Exit - 0 / Print - 1 / Add - 2 / Delete - 3\n");
+        scanf_s("%d", &menu);
+    }
+    return 0;
 }
