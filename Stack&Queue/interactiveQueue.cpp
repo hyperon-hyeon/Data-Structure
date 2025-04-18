5a585480ad3c214555b018b7109bead5a32e203d
hyperon-hyeon <hyperon1122@gmail.com>
2025-04-18 09:35:13 +0900

    interactiveQueue fix

--- Stack&Queue/interactiveQueue.cpp
+++ Stack&Queue/interactiveQueue.cpp
@@ -1,87 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX_SIZE 10
 
 int queue[MAX_SIZE];
-int front = -1;
-int rear = -1;
+int front=-1, rear=-1, count=0;
 
-
-int queue_empty() {
-	if (front <= -1) { return 1; }
-	else { return 0; }
+int queueEmpty(){
+    if (count<=0) return 1;
+    else return 0;
 }
 
-int queue_full() {
-	
-	if (rear >= MAX_SIZE - 1) { 
-		return 1; 
-	}
-	else { return 0; }
+int queueFull(){
+    if (count>=MAX_SIZE) return 1;
+    else return 0;
 }
 
-void print_queue() {
-	if (queue_empty()) { printf("Queue is empty!! \n"); }\
-	else {
-		printf("Queue:");
-		if(front>rear && front!=0){printf(" None\n");}
-		else{
-			for (int i = front; i <= rear; i++) {
-				printf(" %d", queue[i]);
-			}
-		//printf(" -> front: %d, rear: %d\n", front, rear);
-		printf("\n");
-		}
-	}
+void enQueue(int num){
+    if(!queueFull()){
+        if (count==0) {
+            front = 0;
+            rear = 0;
+            count++;
+            queue[rear] = num;
+        }
+        else{
+            rear++;
+            count++;
+            queue[rear%MAX_SIZE] = num;
+        }
+    }
+    else { printf("[Error] Queue is full!\n"); }
 }
 
-void enqueue(int num) {
-	if (front==-1 && rear==-1) {
-		front++;
-		rear++;
-		queue[rear] = num;
-	}
-	else if (!queue_full()) {
-		rear++;
-		queue[rear] = num;
-	}
-	else { printf("ERROR!! Queue is full!\n"); }
+void deQueue(){
+    if (!queueEmpty()) { 
+        front++; 
+        count--;
+    }
+    else { printf("[Error] Queue is empty!\n"); }
 }
 
-void dequeue() {
-	if (front == rear+1 ) {
-		front = -1;
-		rear = -1;
-	}
-	if (!queue_empty()) {
-		front++;
-	}
-	else { printf("ERROR!! Queue is empty\n"); }
+void printQueue(){
+    printf("Queue ->  ");
+    if (queueEmpty()) { printf("None"); }
+    else {
+        for (int i = front; i <= rear; i++) {
+            printf("%d ", queue[i]);
+        }
+    }
+    printf(" \n");
+    
 }
 
 int main() {
-	int menu = -1;
-	int enter = -1;
-	int data = -1;
-	printf("[Menu] Exit - 0 , Print - 1, Push - 2, Pop - 3\n");
-	scanf_s("%d", &menu);
-	
-	while (menu != 0) {
-		if (menu == 1) { print_queue(); }
-		else if (menu == 2) {
-			printf("Enter command: ");
-			scanf_s("%d", &enter);
-			for (int i = 0; i < enter; i++) {
-				printf("Enter data: ");
-				scanf_s("%d", &data);
-				enqueue(data);
-			}
-		}
-		else if (menu == 3) {
-			dequeue();
-		}
-		else { printf("%d is not available!!\n", menu); }
-		printf("\n[Menu] Exit - 0 , Print - 1, Push - 2, Pop - 3\n");
-		scanf_s("%d", &menu);
-	}
-	return 0;
+
+    int menu = -1, inputNum = 0;
+    printf("[Menu] Exit - 0 / Print - 1 / Add - 2 / Delete - 3\n");
+    scanf_s("%d", &menu);
+
+    while (menu != 0) {
+        if (menu == 1) { printQueue(); }
+        else if (menu == 2) {
+            printf("Insert number: ");
+            scanf_s("%d", &inputNum);
+            enQueue(inputNum);
+            printf("[Result] ");
+            printQueue();
+        }
+        else if (menu == 3) {
+            deQueue();
+            printf("[Result] ");
+            printQueue();
+        }
+        else { printf("[Error] %d is not available\n", menu); }
+
+        printf("\n[Menu] Exit - 0 / Print - 1 / Add - 2 / Delete - 3\n");
+        scanf_s("%d", &menu);
+    }
+    return 0;
 }
